743cff0d22253207d80b4320e5596f7dfd8005c4
Avi Rahimov <rahimovavi3@gmail.com>
2023-01-10 00:07:30 +0200

    fill some func

---
+++ nodes.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+typedef struct GRAPH_NODE_ *pnode;
+
+typedef struct edge_ {
+    int weight;
+    pnode endpoint;
+    struct edge_ *next;
+} edge, *pedge;
+
+
+typedef struct GRAPH_NODE_ {
+    int node_num;
+    pedge edges;
+    struct GRAPH_NODE_ *next;
+} node, *pnode;
+
+void free_graph(pnode *head)
+{
+    if()
+}
+
+void build_graph_cmd(pnode *head, int num_of_nodes)
+{
+
+}
+
+void insert_node_cmd(pnode *head)
+{
+
+}
+
+void delete_node_cmd(pnode *head)
+{
+
+}
+
+void printGraph_cmd(pnode head)//for self debug
+{
+
+} 
+
+void deleteGraph_cmd(pnode *head)
+{
+
+}
+
+void shortsPath_cmd(pnode head)
+{
+
+}
+
+void TSP_cmd(pnode head)
+{
+
+}
