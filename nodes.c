0f817fdac73ba78b25ff95875b82d7fa1c087dbd
Avi Rahimov <rahimovavi3@gmail.com>
2023-01-17 01:16:29 +0200

    Changes by aviai

--- nodes.c
+++ nodes.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "nodes.h"
+#include "edges.h"
 
 void free_graph(pnode *head)
 {
@@ -41,12 +42,43 @@ void build_graph_cmd(pnode *head, int num_of_nodes)
 
 }
 
-void insert_node_cmd(pnode *head)
+void insert_node_cmd(pnode *head , int n)
 {
+    int to_link;
+    int weight;
+    pnode current = *head; // creating a new pointer and initialize it with the value of the head of the graph
+    pnode prev = NULL; // while we traverse the list of nodes, prev will be used to keep track of the previous node we have been
 
+    while (current != NULL) { // traverse the list of nodes
+        if (current->node_num == n) { //check if the node we want to insert is already exists
+            printf("The Node %d is already exists. Delete all outgoing edges", n);
+            pedge e = current->edges;
+            while (e != NULL) {
+                pedge temp = e->next;
+                free(e); // deleting all the edges from the node that exist.
+                e = temp;
+            }
+            make_edge(head, n, to_link, weight);
+        } return;
+        //if the node does not exist
+        prev = current;
+        current = current->next;
+    }
+    //if the node does not exist, create new node and add it to the graph
+    pnode newNode = (pnode)malloc(sizeof(node));
+    newNode->node_num = n;
+    newNode->edges = NULL;
+    newNode->next = NULL;
+    if(prev == NULL){
+        *head = newNode;
+    }
+    else{
+        prev->next = newNode;
+    }
+    printf("Node %d was inserted successfully.\n",n);
 }
 
-void delete_node_cmd(pnode *head)
+void delete_node_cmd(pnode *head, int node_to_delete)
 {
     pnode delete_node = (*head), temp = (*head);
     pedge temp_edge;
@@ -75,7 +107,7 @@ void delete_node_cmd(pnode *head)
 void printGraph_cmd(pnode head)//for self debug
 {
 
-} 
+}
 
 void deleteGraph_cmd(pnode *head)
 {
